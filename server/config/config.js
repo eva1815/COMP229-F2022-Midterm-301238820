735e282aee71157806bf01611a970ddd9b518276
eva1815 <evafan1815@gmail.com>
2022-10-24 14:42:03 -0400

    Connected to MongoDB

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,3 @@
-export const MongoURI = "mongodb://localhost:27017/media";
+//export const MongoURI = "mongodb://localhost:27017/media";
 export const Secret = "someSecret";
+export const MongoURI = "mongodb+srv://eva:Canada170613@cluster0.qlp6cqc.mongodb.net/media?retryWrites=true&w=majority";
